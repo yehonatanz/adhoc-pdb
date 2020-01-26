28b8e5ab26dade8b09cb7f6ad22a5ff8a2f564cf
Yehonatan Zecharia <yonti95@gmail.com>
2020-01-26 22:39:15 +0200

    Add system test for CLI

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,39 +1,59 @@
+import contextlib
 import os
 import subprocess
 import sys
 import telnetlib
 import time
+from typing import Iterator, List
 
 import pytest
 
+from adhoc_pdb import cli
+
 
 @pytest.fixture(scope="session")
-def script_path():
+def root_path():
     # type: () -> str
-    path = os.path.join(
-        os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
-        "script_to_debug.py",
-    )
+    path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    assert os.path.isdir(path)
+    return path
+
+
+@pytest.fixture(scope="session")
+def script_path(root_path):
+    # type: (str) -> str
+    path = os.path.join(root_path, "script_to_debug.py")
     assert os.path.exists(path)
     return path
 
 
-@pytest.yield_fixture
-def script(script_path):
+@contextlib.contextmanager
+def process_fixture(args, startup_time=0.1, time_before_sigkill=0.1):
+    # type: (List[str], float, float) -> Iterator[subprocess.Popen]
     proc = subprocess.Popen(
-        [sys.executable, script_path],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        stdin=subprocess.PIPE,
+        args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE,
     )
+    time.sleep(startup_time)
     try:
-        time.sleep(0.1)
         yield proc
     finally:
         proc.terminate()
-        time.sleep(0.002)
+        if proc.poll() is None:
+            time.sleep(time_before_sigkill)
         if proc.poll() is None:
             proc.kill()
+
+
+@pytest.yield_fixture
+def script(script_path):
+    with process_fixture([sys.executable, script_path]) as proc:
+        yield proc
+
+
+@pytest.yield_fixture
+def cli_client(script):
+    with process_fixture([sys.executable, "-m", cli.__name__, str(script.pid)]) as proc:
+        yield proc
 
 
 @pytest.yield_fixture
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,5 @@
-from typing import Union
+from typing import TypeVar, Union
+from typing.io import IO
 
 import click
 import pytest
@@ -42,3 +43,24 @@ def test_cli_fails_on_wrong_pid():
     with pytest.raises(click.UsageError) as e:
         click.Context(cli).invoke(cli, pid=pid)
     assert str(pid) in str(e)
+
+
+T = TypeVar("T", str, bytes)
+
+
+def _read_until(buf, pattern):
+    # type: (IO[T], T) -> T
+    c = buf.read(1)
+    while not c.endswith(pattern):
+        c += buf.read(1)
+    return c
+
+
+def test_cli_happy_flow(script, script_path, cli_client):
+    assert script_path.encode("utf-8") in cli_client.stdout.readline()
+    _read_until(cli_client.stdout, b"(Pdb) ")
+    cli_client.stdin.write(b"b 10\n")
+    cli_client.stdin.flush()
+    breakpoint_response = cli_client.stdout.readline()
+    assert b"Breakpoint 1 at" in breakpoint_response
+    assert breakpoint_response.strip().endswith(b".py:10")
